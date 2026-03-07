7242735fb2d1a46e20ade24a061c27cf75531a03
Nitin <nc8224140@gmail.com>
2026-03-07 22:41:29 +0530

    fake in

--- src/components/NewsLetterBox.jsx
+++ src/components/NewsLetterBox.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 function NewsLetterBox() {
   return (
     <div>NewsLetterBox
-      <h1>hello world </h1>
-      <p>using this one only for the git push will soon delete this one so feel free</p>
-    </div>
+         </div>
   )
 }
 
