374ad28542ff550e6b8dd43c278c4ed5681cc052
Nitin0264 <nc8224140@gmail.com>
2026-09-22 21:06:48 +0530

    Fix Vite configuration for Vercel

--- Admin/vite.config.js
+++ Admin/vite.config.js
@@ -1,18 +1,14 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react(),
-    {
-      name: 'force-exit-after-build',
-      apply: 'build',
-      closeBundle() {
-        setTimeout(() => process.exit(0), 0)
-      }
-    }
   ],
 })
+
+
+
